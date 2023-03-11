e5d396e93472f012bcc50e49e2ac2c4c49fc030f
Astashenko Nikolay <astashenko3@gmail.com>
2023-03-12 02:52:33 +0300

    Бяда

---
+++ Example005_Condition_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+  Console.WriteLite("Ура, это же МАША!");
+}
+else
+{
+  Console.Write("Привет, !");
+  Console.WriteLite(username);
+}
